7d209fe0e20e60e510c692a777f0369d5cf12187
yj120 <yeji3852@daum.net>
2023-06-21 03:31:25 +0900

    add 2controller

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -17,5 +18,34 @@ public class HelloController {
     public String helloMvc(@RequestParam(value = "name",required = true)String name,Model model){
         model.addAttribute("name",name);
         return "hello-templates"; // hello-templates 에서 랜더링
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody // http body 에 직접 넣어주겠다.
+    public String helloString(@RequestParam("name") String name){
+        return "hello " + name; //name 을 어떻게 넣어주냐에 따라 값이 다르게 출력
+        // 이전의 템플릿이랑 다른점은 view가 없음 그냥 문자그대로 넘어감->html tag 없이!
+    }
+
+
+    // 찐 API ----> json 형태로 출력
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name){
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+    static class Hello{
+        private String name;
+
+        public String getName(){
+            return name;
+        };
+
+        public void setName(String name){
+            this.name= name;
+
+        }
     }
 }
